b0117aa8b9a76fc68bbaeff312409b1244eef5e7
alfianaf <alfianaf123@gmail.com>
2018-10-26 14:39:33 +0800

    App-Modul1_Kelompok20

---
+++ Kelompok20/Kelompok20/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Kelompok20;
+using System;
+using Xamarin.Forms;
+
+namespace Kelompok20
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnLogoutButtonClicked(object sender, EventArgs e)
+        {
+            App.IsUserLoggedIn = false;
+            Navigation.InsertPageBefore(new LoginPage(), this);
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ Kelompok20/Kelompok20/Model/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kelompok20
+{
+    public static class UserLogin
+    {
+        public static string Username = "Kelompok20";
+        public static string Password = "password";
+    }
+}
